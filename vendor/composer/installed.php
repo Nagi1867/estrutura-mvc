13cdda74a58e067936d8c271839cfdaaebb2664b
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-15 14:11:58 -0300

    composer e autoload psr-4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gabriel/miniframework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd94988f660ec5f5662369939f9145e53420d7428',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gabriel/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd94988f660ec5f5662369939f9145e53420d7428',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
